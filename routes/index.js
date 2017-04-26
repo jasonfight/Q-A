c87e58a78c2e77c2aab1e6c4ac06e231b457c613
jason <jasonlearning@outlook.com>
2017-04-26 21:28:20 +0800

    modified files
        更新04.26总结文档
        新增express-session和 bcrypt两个第三方库
        在 index.ejs文件中使用if语句

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,80 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database');
 
+var session = require('express-session');
+var bcrypt = require('bcrypt');
+
+const salt = 10;
+var app = express();
+
+app.use(session({
+  resave:true,
+  saveUninitialized:false,
+  secret:'secret',
+  cookie:{
+    maxAge:1000*60*30
+  }
+}));
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('index')
 });
 
+//登录验证页面
 router.post('/',function(req,res,next){
 
     var account = req.body.account;//前端传过来的用户名
     var password = req.body.password;//前端传过来的密码
-    console.log(account);
-    console.log(password);
-    db.User.findOne({account:account},function(err,data){
 
-      if (data != null && account == data.account && password == data.password) {
-          res.render('index')
+    db.User.findOne({account:account},function(err,data){
+      if (data){
+        bcrypt.compare(password,data.password,function(err,hash){
+            console.log(hash);
+            if(hash){
+
+              console.log(req.session,'=================');
+              // req.session.user = account;
+              req.session.user = account;
+              res.redirect('/index');
+            }else{
+              // req.session.messages = '密码错误,请重新输入';
+              res.redirect('/login');
+            }
+        })
       }else{
-          res.render('login',{info:'账号密码错误,请重新输入'})
+        req.session.messages = '账号不存在,请重新输入';
+        res.redirect('/login');
       }
 
-    });
+      // if (data != null && account == data.account && password == data.password) {
+      //     res.render('index')
+      // }else{
+      //     res.render('login',{info:'账号密码错误,请重新输入'})
+      // }
 
+    });
 })
+router.post('/register',function(req,res){
+  console.log('heheheeheh');
+  var user = new db.User(req.body);  // NOTE: ???? req.body 有什么用
+  console.log(user); //可能是数据库中的字段名
+
+  bcrypt.hash(req.body.password,salt,function(err,hash){
+    console.log(hash);
+    user.password = hash;
+    user.save(function(err){
+      res.redirect('/login');
+    })
+  })
+})
+
+router.get('/register',function(req,res,next){
+  res.render('register')
+})
+
 router.get('/tag',function(req,res,next){
   res.render('tag')
 })
@@ -44,9 +96,6 @@ router.get('/problem',function(req,res,next){
   res.render('problem')
 })
 
-router.get('/register',function(req,res,next){
-  res.render('register')
-})
 
 router.get('/login',function(req,res,next){
   res.render('login',{info:''})
