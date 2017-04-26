ef9e4b9bf07f74e6eebc3809c0c99c5a9d7a1abd
jason <jasonlearning@outlook.com>
2017-04-26 09:23:46 +0800

    modified databases files
    	修改：     database/ceshi.js
    	修改：     database/index.js

--- database/ceshi.js
+++ database/ceshi.js
@@ -1,26 +1,45 @@
 var db = require('./index')
 
-// var jason = new db.User({
-//   account:'user',
-//   nickname:'jason',
-//   password:'123',
-//   Q_list:{title:'question.title',content:'q.content'},
-//   like_num:3
-// });
 
-// jason.save(function(err){
+//用户集合存储测试.
+var  user1 = new db.User({
+  account:'user1',
+  nickname:'guohao',
+  password:'123',
+  Q_list:[{q_id:1111111111,title:'提问1'},{q_id:222222222,title:'提问2'}],
+  A_list:{q_id:222222,title:'问题2'},
+  F_list:{u_id:111111111,nickname:'kobe'},
+  like_num:3,
+  be_liked_num:4,
+  col_list:[{q_id:11111111,title:'收藏1'},{q_id:22222,title:'收藏2'}],
+  report_or_be_reported:{report:1,be_reported:2},
+  acticity:100,
+  information:{ age:20,
+                tel:0100000000,
+                email:'user1@126.com',
+                gender:'male'
+              }
+});
+// user1.save(function(err){
 //   if (err) console.log(err);
 //   console.log('保存成功');
 // });
-var  user2 = new db.User({
-  account:'user2',
-  nickname:'guohao',
-  password:'123',
-  Q_list:{q_id:1111111111,content:'q.content'},
-  A_list:{title:'Answer.title',content:'Answer.content'},
-  like_num:3
+
+//问题集合存储测试
+var que = new db.Question({
+  title:'问题1的标题',
+  content:'问题1的内容',
+  author:{u_id:1111111,nickname:'jason'},
+  P_date:'2017-02-21',
+  tag:['python','html'],
+  reading_num:100,
+  adopted:true,
+  final_reply_or_modify_time:'2017-02-21',
+  A_list:[{A_id:11111},{A_id:12121212}],
+  be_liked_num:200
 });
-user2.save(function(err){
+
+que.save(function(err){
   if (err) console.log(err);
-  console.log('保存成功');
+  console.log('问题列表保存成功');
 })
--- database/index.js
+++ database/index.js
@@ -12,16 +12,16 @@ var userSchema = new Schema({
   account:String,//账号
   nickname:String,//昵称
   password:String,//密码
-  Q_list:{q_id:Schema.Types.ObjectId,title:String},//问题列表
-  A_list:{q_id:Schema.Types.ObjectId,title:String},//回答列表
-  F_list:{u_id:Schema.Types.ObjectId,nickname:String},//关注列表
+  Q_list:[{q_id:String,title:String}],//问题列表
+  A_list:[{q_id:String,title:String}],//回答列表
+  F_list:[{u_id:String,nickname:String}],//关注列表
   like_num:Number,//点赞数
   be_liked_num:Number,//被点赞数
-  col_list:{q_id:Schema.Types.ObjectId,title:String},//收藏列表
+  col_list:[{q_id:String,title:String}],//收藏列表
   report_or_be_reported:{report:Number,be_reported:Number},//举报或被举报次数
-  acticity:Number//活跃度
+  acticity:Number,//活跃度
   information:{age:Number,//个人信息
-              tel:Number,
+              tel:String,
               email:String,
               gender:String,
              }
@@ -29,38 +29,39 @@ var userSchema = new Schema({
 
 module.exports.User = mongoose.model('User',userSchema);
 
+
 var Q_Schema = new Schema({
   title:String,//标题
   content:String,//内容
-  author:{u-id:Schema.Types.ObjectId,nickname:String},//作者
+  author:{u_id:String,nickname:String},//作者
   P_date:{type:Date,default:Date.now},//发表时间
   tag:Array,//标签
   reading_num:Number,//浏览数量
   adopted:Boolean,//是否被采纳
   final_reply_or_modify_time:Date,//最后修改或回答时间
-  respondent:{u_id:Schema.Types.ObjectId,nickname:String},//回答人
-  A_list{A_id:Schema.Types.ObjectId},//回答列表
+  A_list:[{A_id:String}],//回答列表
   be_liked_num:Number//被点赞数量
-
 });
+
 module.exports.Question = mongoose.model('Question',Q_Schema);
+
+
 
 var A_Schema = new Schema({
   content:String,//回答内容
   date:Date,//回答日期
-  respondent:{u_id:Schema.Types.ObjectId,nickname:String},//回答人
+  respondent:[{u_id:String,nickname:String}],//回答人
   adopted:Boolean,//是否被采纳
-  be_liked_num:Number//被点赞数
-  que{q_id:Schema.Types.ObjectId,title:String},//问题列表
+  be_liked_num:Number,//被点赞数
+  que:[{q_id:String,title:String}],//问题列表
 });
 
 module.exports.Answer = mongoose.model('Answer',A_Schema);
 
 var Tag_Schema = new Schema({
-  class:String,//标签所在分类
-  name:String,//标签名
-  content:String,//标签简介
-  Q_list:{q_id:Schema.Types.ObjectId,title:String}//相关问题列表
+  title:String,//标签所在分类
+  tag:String,//标签名
+  information:String,//标签简介
 });
 
 module.exports.Tag = mongoose.model('Tag',Tag_Schema);
